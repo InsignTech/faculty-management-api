e03f75aaf74c9a8775b543b6f1129804c17b7743
mhdyaseen8841 <mhdyaseen.official@gmail.com>
2026-07-14 18:22:05 +0530

    refactor: remove obsolete development scripts and database utility files

--- scratch/check_import.js
+++
@@ -1,7 +0,0 @@
-try {
-    const app = require('../app');
-    console.log("Backend app.js imported successfully!");
-} catch (e) {
-    console.error("Error importing app.js:", e);
-}
-process.exit(0);
